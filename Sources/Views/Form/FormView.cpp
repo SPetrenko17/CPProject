539f7eed4e873c532a1664d91b104ba841e19c65
SPetrenko17 <SPetrenko17@yandex.ru>
2019-12-17 18:38:09 +0300

    minimum server and static

---
+++ Sources/Views/Form/FormView.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Сергей Петренко on 2019-12-17.
+//
+
+#include "FormView.h"
---
+++ Sources/Views/Form/FormView.h
@@ -0,0 +1,14 @@
+//
+// Created by Сергей Петренко on 2019-12-17.
+//
+
+#ifndef CPPROJECT_FORMVIEW_H
+#define CPPROJECT_FORMVIEW_H
+
+
+class FormView {
+
+};
+
+
+#endif //CPPROJECT_FORMVIEW_H
